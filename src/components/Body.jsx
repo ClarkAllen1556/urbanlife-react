05508b86f18dad22ead4cf39c1cfaed7e9107ca4
AC1556 <allenclark@u.boisestate.edu>
2022-04-29 21:24:09 +0900

    Misc: playing around with tailwind

---
+++ src/components/Body.jsx
@@ -0,0 +1,8 @@
+export default function Body() {
+  return (
+    <div className="bg-red-300 ml-5 mr-5 p-2 border-2 rounded-md border-blue-600">
+      <p> Hello there! My name is Allen Clark! </p>
+      <p> I'm a software engineer located in Tokyo, Japan! </p>
+    </div>
+  );
+}
