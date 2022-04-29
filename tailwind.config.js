0c93d990c0c8f15d90ba75f09f13759ebd354cef
AC1556 <allenclark@u.boisestate.edu>
2022-04-29 21:23:26 +0900

    Build (tailwind): setting up tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'sol-tan-1': '#fdf6e3',
+      },
+    },
+  },
+  plugins: [],
+};
