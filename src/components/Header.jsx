2628ac28f367e83825db3fee3e461be15d173ce1
AC1556 <allenclark@u.boisestate.edu>
2022-05-01 23:39:43 +0900

    Removing logo from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
-import logUrl from '/assets/logo.png';
-
 export default function Header() {
   return (
     <div
-      className=" flex min-h-min items-center gap-5 mb-5 ml-5 mr-5 p-2 border-b-2
-        border-l-2 border-r-2 rounded-b-md border-b-blue-600 border-l-blue-600
-       border-r-blue-600 bg-slate-400
+      className="
+        min-h-min items-center gap-5 mb-5 ml-5 mr-5 p-2 pl-5 border-2 border-t-0
+        rounded-b-md text-3xl
+        border-sol-blue-1 bg-sol-tan-1
       "
     >
-      <img className="scale-75" src={logUrl} />
-      <h1 className=""> The Allen C. @ The Urbanlife </h1>
+      {/* <img className="scale-75" src={logUrl} /> */}
+      <span className="text-card-title-red">TheAllenC@TheUrbanlife </span>
+      <span className="font-semibold">~$</span>
     </div>
   );
 }
