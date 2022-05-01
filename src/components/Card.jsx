4014d3f8ab966515b7de01a4dd9bf46d214f4c74
AC1556 <allenclark@u.boisestate.edu>
2022-05-01 18:21:54 +0900

    Adding flavor to card titles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
 export default function Card({ title, children }) {
   return (
-    <div className="min-h-min p-5 shadow-md shadow-gray-500 bg-white border-2 border-blue-600 rounded-md">
-      <h1 className="text-2xl"> {title} </h1>
+    <div className="min-h-min p-5 rounded-md shadow-md shadow-gray-500 bg-white border-2 border-blue-600">
+      <h1 className="text-2xl">
+        <span className="font-semibold">~$ </span>
+        <span className="text-card-title-red">{title}</span>
+      </h1>
       <div className="flex grow ">{children}</div>
     </div>
   );
