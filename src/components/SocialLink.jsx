cc139ed8894b9ee89dfa3c5be358996974bb4a19
AC1556 <allenclark@u.boisestate.edu>
2022-05-01 16:19:12 +0900

    Adding `SocialLink` component

---
+++ src/components/SocialLink.jsx
@@ -0,0 +1,14 @@
+export default function SocialLink({ social, children }) {
+  return (
+    <a
+      className="w-full flex justify-center py-2 px-4 border mb-auto
+        border-transparent text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2
+      text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500
+      "
+      href={social.link}
+      title={social.tag}
+    >
+      {children}
+    </a>
+  );
+}
--- src/components/SocialsList.jsx
+++ src/components/SocialsList.jsx
@@ -1,19 +1,16 @@
 import { socials } from '../meta/Socials.json';
+import SocialLink from './SocialLink';
 import { FiGithub, FiLinkedin } from 'react-icons/fi';
 
 export default function SocialsList() {
-  console.log(socials);
   return (
     <div className="flex">
-      <div className="bg-green-200 rounded-md">
-        <a
-          className="min-w-fit"
-          href={socials.github.link}
-          title={socials.github.tag}
-        >
-          {socials.github.tag}
-        </a>
-      </div>
+      <SocialLink social={socials.linkedin}>
+        <FiLinkedin />
+      </SocialLink>
+      <SocialLink social={socials.github}>
+        <FiGithub />
+      </SocialLink>
     </div>
   );
 }
