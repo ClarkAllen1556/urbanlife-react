b9cf2c22e6c706d63a6315a2929fa7fba09d3b2b
AC1556 <allenclark@u.boisestate.edu>
2022-05-01 18:03:19 +0900

    Using props for card title

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
-import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header';
 import Body from './components/Body';
 import Hero from './components/Hero';
+import About from './components/About';
 import Card from './components/Card';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="mx:container flex-col h-screen bg-sol-tan-1">
+    <div className="mx:container flex-col pb-10 min-h-full bg-sol-tan-1">
       <Header />
       <Body>
         <Hero />
 
-        <div className="flex gap-2 ml-5 mr-5">
-          <Card>
-            <div>
-              <h1 className="text-2xl"> Welcome to my page! </h1>
-              <p> Hello there! My name is allen clark! </p>
-            </div>
-          </Card>
-          <Card>
-            <div>
-              <h1 className="text-2xl"> Welcome to my page! </h1>
-              <p> Hello there! My name is allen clark! </p>
-            </div>
-          </Card>
+        <div className="flex ml-5 mr-5">
+          <div className="w-5/6">
+            <Card title="Experience!">
+              <Resume />
+            </Card>
+          </div>
+
+          <div className="flex flex-col gap-2 ml-5 mr-5">
+            <Card title="Welcome to my page!">
+              <p>
+                {' '}
+                Hey there! My name is Allen Clark. I'm software engineer located
+                in Tokyo, Japan. I'm enthusiastic about all things tech and
+                open-source and currently, loving Elixir and Phoenix.{' '}
+              </p>
+            </Card>
+
+            <Card title="More about me!">
+              <About />
+            </Card>
+
+            <Card title="Contact me!">
+              <Contact />
+            </Card>
+          </div>
         </div>
       </Body>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,8 @@
-export default function Card({ children }) {
+export default function Card({ title, children }) {
   return (
-    <div className="flex grow rounded-md p-5 shadow-md shadow-gray-500 bg-white border-2 border-blue-600">
-      {children}
+    <div className="min-h-min p-5 shadow-md shadow-gray-500 bg-white border-2 border-blue-600 rounded-md">
+      <h1 className="text-2xl"> {title} </h1>
+      <div className="flex grow ">{children}</div>
     </div>
   );
 }
